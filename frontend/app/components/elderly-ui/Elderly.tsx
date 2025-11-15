75fd638c89950cbef9220cfb2819976db929e32a
Petu <pojp07@gmail.com>
2025-11-15 11:25:54 +0200

    feat(IntroText): create base for intro text

--- frontend/app/components/elderly-ui/Elderly.tsx
+++ frontend/app/components/elderly-ui/Elderly.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState } from 'react';
+import IntroText from './IntroText';
 import TalkButton from "./TalkButton"
 import Listening from "./Listening"
 
@@ -8,19 +9,17 @@ import Listening from "./Listening"
 export default function Elderly() {
     const [listening, setListening] = useState(false);
 
-    if (listening) {
-        return (
-            <div>
-                <h1 className="text-7xl font-bold"> Check-in companion </h1>
-                <Listening onStop={() => setListening(false)} />
-            </div>
-        )
-    }
-
     return (
         <div>
             <h1 className="text-7xl font-bold"> Check-in companion </h1>
-            <TalkButton onStart={() => setListening(true)} />
+            <IntroText listening={listening} />
+
+            <div className="flex flex-col items-center justify-center mt-30 ">
+                {listening
+                    ? <Listening onStop={() => setListening(false)} />
+                    : <TalkButton onStart={() => setListening(true)} />
+                }
+            </div>
         </div>
     )
 }
--- frontend/app/components/elderly-ui/TalkButton.tsx
+++ frontend/app/components/elderly-ui/TalkButton.tsx
@@ -4,12 +4,10 @@ type Props = {
 
 export default function TalkButton({ onStart }: Props) {
     return (
-        <div className="flex justify-center mt-80">
-            <button
-                className="bg-blue-500 hover:bg-blue-700 size-90 text-white text-5xl font-bold rounded-full"
-                onClick={onStart}>
-                Talk to<br />your companion
-            </button>
-        </div>
+        <button
+            className="bg-blue-500 hover:bg-blue-700 size-90 text-white text-5xl font-bold rounded-full"
+            onClick={onStart}>
+            Talk to<br />your companion
+        </button>
     );
 }
