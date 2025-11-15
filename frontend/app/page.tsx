3462e10bd6dc990feb96dbca9c37c62b00ceb180
Iliana Dulina <ilyana.dulina07@gmail.com>
2025-11-15 14:47:59 +0200

    puolvalmis page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,18 @@
 import Elderly from './components/elderly-ui/Elderly';
 import Family from './components/family-ui/Family';
 
+import { useState } from 'react';
+
 export default function Home() {
+  const [mode, setMode] = useState<string>('elderly');
+
   return (
     <div className="m-3">
       <h1 className="text-7xl font-bold"> Check-in companion </h1>
-      <Elderly />
+        {mode == 'elderly'
+          ? <Elderly changeMode={() => setMode('elderly')} />
+          : <Family changeMode{() => setMode('family')} />
+        }
     </div>
   );
 }
