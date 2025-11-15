810faaa794b990f1da322b1bf7683fa1bd489605
Petu <pojp07@gmail.com>
2025-11-15 14:30:02 +0200

    feat(app): add base for family dashboard (ui)

--- frontend/app/components/elderly-ui/Elderly.tsx
+++ frontend/app/components/elderly-ui/Elderly.tsx
@@ -7,11 +7,10 @@ import Listening from './Listening';
 
 
 export default function Elderly() {
-    const [listening, setListening] = useState(false);
+    const [listening, setListening] = useState<boolean>(false);
 
     return (
         <div>
-            <h1 className="text-7xl font-bold"> Check-in companion </h1>
             <Greeting listening={listening} />
 
             <div className="flex flex-col items-center justify-center mt-30 ">
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
-import Elderly from "./components/elderly-ui/Elderly";
+import Elderly from './components/elderly-ui/Elderly';
+import Family from './components/family-ui/Family';
 
 export default function Home() {
   return (
     <div className="m-3">
+      <h1 className="text-7xl font-bold"> Check-in companion </h1>
       <Elderly />
     </div>
   );
