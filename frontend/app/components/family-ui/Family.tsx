e4a77959c8d6e4733a1461c0644b800843f3b1db
Petu <pojp07@gmail.com>
2025-11-15 23:57:12 +0200

    feat(family): add example alert for family ui

--- frontend/app/components/family-ui/Family.tsx
+++ frontend/app/components/family-ui/Family.tsx
@@ -1,3 +1,4 @@
+import Alert from './Alert';
 import ModeButton from '../shared/ModeButton';
 
 type Props = {
@@ -8,6 +9,10 @@ export default function Family({changeMode}: Props) {
     return (
         <div>
             <h1 className="text-7xl font-bold"> Family dashboard </h1>
+
+            {/* for alert in alerts (database?), print alert */}
+            <Alert mood={2} energy={2} loneliness={2} risk={2} summary="User fell in the woods and is in pain with low mood and energy but not feeling lonely; high risk due to the fall" />
+
             <ModeButton changeMode={changeMode} />
         </div>
     );
